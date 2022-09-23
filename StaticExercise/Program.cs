0e19aba1ca85e3aecddc8787f5430b1634c94490
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-23 17:15:36 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using StaticExercise;
+
+TempConverter.FahrenheitToCelsius(85);
+TempConverter.FahrenheitToCelsius(30);
+
+TempConverter.CelsiusToFahrenheit(25);
+TempConverter.CelsiusToFahrenheit(50);
+
+
+//Tests
+//Given 85 F should return 29.444 c
+//Given 30 F should return -1.11 C
+//Given 25 C should return 77 F
+//Given 50 C should return 122 F
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static void FahrenheitToCelsius (double tempF) 
+        {
+            
+            double tempC = (tempF - 32) *5/9;
+            tempC = Math.Round(tempC, 2);
+            Console.WriteLine ($"{tempF} degrees fahrenheit converts to {tempC} degrees celsius");
+        }
+
+        public static void CelsiusToFahrenheit (double tempC)
+        {
+            double tempF = (tempC * 1.8) + 32;
+            //return tempF;
+            Console.WriteLine($"{tempC} degrees celsius converts to {tempF} degrees fahrenheit");
+        }
+
+    }
+}
